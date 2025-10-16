830fc7315c1c8034d6254ff69ef2196787c67021
argeulimbo <argeusousaptbr@gmail.com>
2025-10-16 11:22:36 -0300

    init: inicializando projeto

---
+++ src/main/java/com/br/lojix/LojixApplication.java
@@ -0,0 +1,13 @@
+package com.br.lojix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojixApplication.class, args);
+	}
+
+}
