fd6c4f5c73ad3349c4232aed89bcfc6df70bfa90
argeulimbo <argeusousaptbr@gmail.com>
2025-11-13 15:51:54 -0300

    feat: created jwtutil.java && jwtauthenticationfilter

---
+++ src/main/java/com/br/lojix/security/JWTAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.br.lojix.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JWTAuthenticationFilter  extends UsernamePasswordAuthenticationFilter {
+	
+	private AuthenticationManager authenticationManager;
+	private JWTUtil jwtUtil;
+	
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+		super();
+		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+	}
+	
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		return super.attemptAuthentication(request, response);
+	}
+
+}
--- src/main/java/com/br/lojix/service/DbService.java
+++ src/main/java/com/br/lojix/service/DbService.java
@@ -1,27 +1,9 @@
 package com.br.lojix.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import com.br.lojix.repository.FornecedorRepository;
-import com.br.lojix.repository.PedidoRepository;
-import com.br.lojix.repository.ProdutoRepository;
 
 @Service
 public class DbService {
-	
-	@Autowired
-	private FornecedorRepository fornecedorRepository;
-	
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	
-	@Autowired
-	private BCryptPasswordEncoder encoder;
 	
 	
 	public void instanciaDB() {
